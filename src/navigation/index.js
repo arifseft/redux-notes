9dcc76774886f2efedf71cc4b5e86609f9b7307d
M. Arif Sefrianto <arifsefrianto@gmail.com>
2020-05-22 13:43:51 +0700

    first commit

---
+++ src/navigation/index.js
@@ -0,0 +1,21 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import ViewNotes from "../screens/ViewNotes";
+import AddNotes from "../screens/AddNotes";
+
+const StackNavigator = createStackNavigator(
+  {
+    ViewNotes: {
+      screen: ViewNotes,
+    },
+    AddNotes: {
+      screen: AddNotes,
+    },
+  },
+  {
+    initialRouteName: "ViewNotes",
+    headerMode: "none",
+  }
+);
+
+export default createAppContainer(StackNavigator);
